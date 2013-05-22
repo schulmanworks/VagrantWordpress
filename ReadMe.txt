3e53d3809dd5a77399b8e8ce6477da8dd1636be4
ryan <ryan@ryan-desktop.(none)>
2013-05-22 16:09:05 -0600

    added VagrantSetup2 and ReadMe

---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+To begin installation, open a terminal and type:
+chmod+x VagrantSetup1.sh
+./VagrantSetup1.sh
---
+++ VagrantSetup2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Continued setup of Vagrant box
+sudo apt-get -f install 
+#Install apache
+sudo apt-get install apache2
+sudo apt-get update
