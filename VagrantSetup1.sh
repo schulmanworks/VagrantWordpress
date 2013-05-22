3eef1cc69d66195005b8aa3a86ce9949c8b0cb9a
ryan <ryan@ryan-desktop.(none)>
2013-05-22 16:06:22 -0600

    test

---
+++ VagrantSetup1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Run this in an empty directory to setup Vagrant for use with wordpress
+#Define user and host 
+HOST=$(hostname)
+USR=$user
+echo $USR@$HOST
+#prepare VagrantSetup2.sh
+chmod +x VagrantSetup2.sh
+#Create Vagrantfile
+vagrant init precise64 http://files.vagrantup.com/precise64.box
+vagrant up
+#Enter vagrant and run VagrantSetup2.sh
+vagrant ssh
+echo penis
+
